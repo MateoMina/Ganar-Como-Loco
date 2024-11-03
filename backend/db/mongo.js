c37b5196cbbe2392f44354067b81240e337d973e
MateoMina <mateominaclavijo12@gmail.com>
2024-11-03 14:27:25 -0500

    Oculta las URL de mongo

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -1,10 +1,10 @@
 const { connect, set } = require('mongoose');
 
-const DB_URI = 'mongodb+srv://jhonier:13052002@restaurant.hluhpeu.mongodb.net/rifas?retryWrites=true&w=majority&appName=restaurant'
+const DB_URI = process.env.MONGODB_URI;
 
 const dbInit = async () => {
     set("strictQuery", false);
-    await connect(`${DB_URI}`);
+    await connect(DB_URI);
 };
 
 module.exports = {
