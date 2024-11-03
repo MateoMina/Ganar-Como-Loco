1ee0ee20e05c2d7427534255546d4079afb4de25
MateoMina <mateominaclavijo12@gmail.com>
2024-11-03 15:14:02 -0500

    v5

--- parcial/src/context/constants.js
+++ parcial/src/context/constants.js
@@ -1,5 +1,4 @@
 //export const apiUrl = 'http://localhost:4000/'
-//export const apiUrl = 'https://ganar-como-loco-jli8nm32n-mateos-projects-c6dfa612.vercel.app/'
 export const apiUrl = process.env.NODE_ENV === 'production'
   ? 'https://ganar-como-loco.vercel.app/'
   : 'http://localhost:4000/';
