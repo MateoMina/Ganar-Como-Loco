1a29d2db9d7ed5b68214f9ec755fcf395c9b7611
MateoMina <mateominaclavijo12@gmail.com>
2024-11-03 14:44:44 -0500

    auth

--- parcial/src/context/constants.js
+++ parcial/src/context/constants.js
@@ -1,5 +1,5 @@
-//export const apiUrl = 'http://localhost:4000/'
+export const apiUrl = 'http://localhost:4000/'
 // export const apiUrl = 'https://backendrifas.vercel.app/'
-export const apiUrl = process.env.NODE_ENV === 'production' 
-  ? '/auth/' 
-  : 'http://localhost:4000/';
+//export const apiUrl = process.env.NODE_ENV === 'production' 
+//  ? '/auth/' 
+//  : 'http://localhost:4000/';
