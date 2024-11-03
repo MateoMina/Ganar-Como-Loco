e009d414e8616d5d4a9f2b3e13e82e55b5950524
MateoMina <mateominaclavijo12@gmail.com>
2024-11-03 14:54:19 -0500

    prueba

--- parcial/src/context/constants.js
+++ parcial/src/context/constants.js
@@ -1,5 +1,6 @@
 //export const apiUrl = 'http://localhost:4000/'
-export const apiUrl = 'https://ganar-como-loco.vercel.app/'
+export const apiUrl = 'https://ganar-como-loco-jli8nm32n-mateos-projects-c6dfa612.vercel.app'
 //export const apiUrl = process.env.NODE_ENV === 'production' 
 //  ? '/auth/' 
-//  : 'http://localhost:4000/';
+//  : 'https://ganar-como-loco.vercel.app/';
+
