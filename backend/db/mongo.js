899e667c101231579463023c77f1c1b0ba9e193c
MateoMina <mateominaclavijo12@gmail.com>
2024-11-03 15:43:39 -0500

    v7

--- backend/db/mongo.js
+++ backend/db/mongo.js
@@ -1,7 +1,7 @@
 const { connect, set } = require('mongoose');
 
-const DB_URI = process.env.MONGODB_URI;
-//const DB_URI = 'mongodb+srv://jhonier:13052002@restaurant.hluhpeu.mongodb.net/rifas?retryWrites=true&w=majority&appName=restaurant'
+//const DB_URI = process.env.MONGODB_URI;
+const DB_URI = 'mongodb+srv://jhonier:13052002@restaurant.hluhpeu.mongodb.net/rifas?retryWrites=true&w=majority&appName=restaurant'
 
 const dbInit = async () => {
     set("strictQuery", false);
